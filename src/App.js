ac0bf86b2b6483a75b12c24bc91f1a6fd9c8b6f7
Maximizer03 <yuvraj.neema.cd.che19@itbhu.ac.in>
2022-01-26 16:42:51 +0530

    Latest Application

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Textform from "./components/Textform";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-	const [mode, setMode] = useState("light");
+	const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
 	const [alert, setAlert] = useState(null);
 
 	const showAlert = (message, type) => {
 		setAlert({
-			message: message,
+			msg: message,
 			type: type,
 		});
 		setTimeout(() => {
 			setAlert(null);
 		}, 1500);
 	};
+
 	const toggleMode = () => {
-		if (mode === "dark") {
+		if (mode === "light") {
+			setMode("dark");
+			document.body.style.backgroundColor = "#042743";
+			showAlert("Dark mode has been enabled", "success");
+		} else {
 			setMode("light");
 			document.body.style.backgroundColor = "white";
 			showAlert("Light mode has been enabled", "success");
-			document.title = "Text Utils Light Mode";
-		} else {
-			setMode("dark");
-			document.body.style.backgroundColor = "#233142";
-			showAlert("Dark mode has been enabled", "success");
-			document.title = "Text Utils Dark Mode";
 		}
 	};
 	return (
 		<>
-			{/* <Router> */}
-			<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-			<Alert alert={alert} />
-			<div className="container my-3">
-				{/* <Switch> */}
-				{/* <Route exact path="/about"> */}
-				{/* <About /> */}
-				{/* </Route> */}
-				{/* <Route exact path="/"> */}
-				<Textform heading="TextBox" mode={mode} showAlert={showAlert} />
-				{/* </Route> */}
-				{/* </Switch> */}
-			</div>
-			{/* </Router> */}
+			<Router>
+				<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+				<Alert alert={alert} />
+				<div className="container my-3">
+					<Switch>
+						{/* /users --> Component 1
+        /users/home --> Component 2 */}
+						<Route exact path="/about">
+							<About mode={mode} />
+						</Route>
+						<Route exact path="/">
+							<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
 		</>
 	);
 }
